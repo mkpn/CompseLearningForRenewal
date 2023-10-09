f41e72ac15c22d8032e6c2c5db40a3509fadd613
neesan <exp.ym.lvl@gmail.com>
2023-10-09 19:28:11 +0900

    Initial commit

---
+++ app/src/main/java/com/neesan/compselearningforrenewal/friend/FriendScreen.kt
@@ -0,0 +1,9 @@
+package com.neesan.compselearningforrenewal.friend
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FriendScreen() {
+    Text(text = "Friend Screen")
+}
