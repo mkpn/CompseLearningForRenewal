f8ba4ccd930ee19ef6d1e860ff41830e61613d42
neesan <exp.ym.lvl@gmail.com>
2023-10-15 18:41:07 +0900

    SplashScreenを作成

--- app/src/main/java/com/neesan/compselearningforrenewal/MyApp.kt
+++ app/src/main/java/com/neesan/compselearningforrenewal/MyApp.kt
@@ -8,14 +8,20 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.neesan.compselearningforrenewal.content.ContentScreen
 import com.neesan.compselearningforrenewal.home.HomeScreen
+import com.neesan.compselearningforrenewal.splash.SplashScreen
 
 @Composable
 fun MyApp() {
     val navController = rememberNavController()
     NavHost(
         navController,
-        startDestination = AppDestinations.HOME_ROUTE,
+        startDestination = AppDestinations.SPLASH,
     ) {
+        composable(AppDestinations.SPLASH) {
+            SplashScreen(onInitFinish = {
+                navController.navigate(AppDestinations.HOME_ROUTE)
+            })
+        }
         composable(AppDestinations.HOME_ROUTE) {
             HomeScreen(
                 onContentSelected = { contentId ->
@@ -37,6 +43,7 @@ fun MyApp() {
 }
 
 object AppDestinations {
+    const val SPLASH = "splash"
     const val HOME_ROUTE = "home"
     const val CONTENT_DETAIL_ROUTE = "content"
     const val CONTENT_ID_KEY = "contentId"
