45a3c13d499a1343d41495c317965582f322cf2c
neesan <exp.ym.lvl@gmail.com>
2023-11-06 00:55:11 +0900

    開閉するように変更完了

--- app/src/main/java/com/neesan/compselearningforrenewal/checkboxGroup/CheckBoxGroupScreen.kt
+++ app/src/main/java/com/neesan/compselearningforrenewal/checkboxGroup/CheckBoxGroupScreen.kt
@@ -1,13 +1,21 @@
 package com.neesan.compselearningforrenewal.checkboxGroup
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -32,30 +40,43 @@ fun CheckboxGroupScreen() {
 }
 
 @Composable
-fun CheckBoxGroupContent(
+private fun CheckBoxGroupContent(
     checkboxItemList: List<CheckboxItem>
 ) {
     val modifiedCheckboxItemList = remember { mutableStateListOf(*checkboxItemList.toTypedArray()) }
+    var isListExpanded by remember { mutableStateOf(false) }
 
     val parentState = modifiedCheckboxItemList.all { it.isChecked }
     Column {
-        Checkbox(
-            checked = parentState,
-            onCheckedChange = { isChecked ->
-                modifiedCheckboxItemList.replaceAll { it.copy(isChecked = isChecked) }
-            }
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = parentState,
+                onCheckedChange = { isChecked ->
+                    modifiedCheckboxItemList.replaceAll { it.copy(isChecked = isChecked) }
+                }
+            )
+            Spacer(Modifier.size(16.dp))
+            Text(
+                modifier = Modifier.clickable { isListExpanded = !isListExpanded },
+                text = if (isListExpanded) "Close" else "Open"
+            )
+        }
 
-        Spacer(Modifier.size(25.dp))
-        Column(Modifier.padding(10.dp, 0.dp, 0.dp, 0.dp)) {
-            modifiedCheckboxItemList.forEachIndexed { index, checkboxItem ->
-                Checkbox(
-                    checked = checkboxItem.isChecked,
-                    onCheckedChange = { isChecked ->
-                        modifiedCheckboxItemList[index] = checkboxItem.copy(isChecked = isChecked)
+        if (isListExpanded) {
+            LazyColumn(modifier = Modifier.padding(start = 10.dp)) {
+                items(checkboxItemList.size) { index ->
+                    val item = modifiedCheckboxItemList[index]
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = item.isChecked,
+                            onCheckedChange = { isChecked ->
+                                modifiedCheckboxItemList[index] = item.copy(isChecked = isChecked)
+                            }
+                        )
+                        Spacer(Modifier.size(16.dp))
+                        Text(text = item.label)
                     }
-                )
-                Spacer(Modifier.size(25.dp))
+                }
             }
         }
     }
